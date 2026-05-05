04d23479b262129d8ac93207922925ecff1c8e7b
WackyWalt <waltonsurratt@gmail.com>
2026-05-05 00:13:57 -0400

    Updated the license & copyright information. Added icon to project.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Surratt Solutions")]
 [assembly: AssemblyProduct("IniFileParser")]
-[assembly: AssemblyCopyright("Copyright © 2026 Surratt Solutions")]
+[assembly: AssemblyCopyright("Copyright © 2026 Walton Surratt")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
